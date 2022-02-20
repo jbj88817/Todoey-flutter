dc1e2ed3c7f882192d40055062442911a2b0c0b5
Bojie Jiang <jiangbojie@gmail.com>
2022-02-20 10:10:20 -0800

    Hoist state

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('this is the task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (check) {},
+      title: Text(
+        'this is the task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+        isChecked: isChecked,
+        onChanged: (check) {
+          setState(() {
+            isChecked = check ?? false;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({Key? key, required this.isChecked, this.onChanged})
+      : super(key: key);
+
+  final bool isChecked;
+  final ValueChanged<bool?>? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (check) {
+        onChanged?.call(check);
+      },
     );
   }
 }
